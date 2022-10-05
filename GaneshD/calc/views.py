9df7400ce41439585b57344af4227209037d1824
Rohan-Swift <sonurohan27@gmail.com>
2022-10-05 08:46:30 +0530

    first commit

---
+++ GaneshD/calc/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main(request):
+    return render(request,'main.html')
+
+def login(request):
+    return render(request,'login.html')
